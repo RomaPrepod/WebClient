bd4b2ac67ec2671a2e67ded5ced1bff618ac6882
Roman Shirokov <shirokovroma@gmail.com>
2017-01-24 00:21:08 +0700

    Top panel styled Fixed layout issues

--- src/app/components/top-menu/top-menu.component.ts
+++ src/app/components/top-menu/top-menu.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+let login = true;
+
 @Component({
   selector: 'rp-top-menu',
   templateUrl: './top-menu.component.html',
-  styleUrls: ['./top-menu.component.css']
+  styleUrls: ['./top-menu.component.scss']
 })
 export class TopMenuComponent implements OnInit {
 
